d681779fe06bad95b594fa6e29aea43f288a1235
minkenovai <146651347+minkenovai@users.noreply.github.com>
2024-03-01 04:40:16 +0600

    Update db.js

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const { Pool } = require('pg');
 const pool = new Pool({
-    user: "postgres",
-    password: "Alish2004",
-    host: "localhost",
+    user: "inkar_finalweb_user",
+    password: "2CleSi522CNx0mxN70m0N3tnv4QOxkJD",
+    host: "dpg-cnggc0f79t8c73a8clag-a.oregon-postgres.render.com",
     port: 5432,
-    database: "finalMinkenova",
+    database: "inkar_finalweb",
 });
 module.exports = pool;
