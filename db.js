a976ec560852789c5318a7dc2e6f283e7f9fb3d2
minkenovai <146651347+minkenovai@users.noreply.github.com>
2024-03-01 04:43:11 +0600

    Update db.js22

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
     user: "inkar_finalweb_user",
     password: "2CleSi522CNx0mxN70m0N3tnv4QOxkJD",
-    host: "dpg-cnggc0f79t8c73a8clag-a.oregon-postgres.render.com",
+    host: "cnggc0f79t8c73a8clag-a.oregon-postgres.render.com",
     port: 5432,
     database: "inkar_finalweb",
 });
